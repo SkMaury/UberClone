99846db01df9d54ed05645eb03e57d42f896a648
SkMaury <SkMaury@github.com>
2018-03-04 18:15:19 +0530

    Added SignIn and SignUp functionality

--- app/src/main/java/android/skmaury/com/uberclone/MainActivity.java
+++ app/src/main/java/android/skmaury/com/uberclone/MainActivity.java
@@ -1,13 +1,264 @@
 package android.skmaury.com.uberclone;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.skmaury.com.uberclone.Model.User;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.rengwuxian.materialedittext.MaterialEditText;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnSignIn, btnRegister;
+    RelativeLayout rootLayout;
+    FirebaseAuth auth;
+    FirebaseDatabase database;
+    DatabaseReference users;
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        /* Add calligraphy before setContentView()*/
+        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+                                            .setDefaultFontPath("fonts/Arkhip_font.ttf")
+                                            .setFontAttrId(R.attr.fontPath)
+                                            .build());
         setContentView(R.layout.activity_main);
+
+        /*Init FireBase*/
+        auth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+        users = database.getReference("Users");
+
+        btnSignIn = findViewById(R.id.btnSign);
+        btnRegister = findViewById(R.id.btnRegister);
+        rootLayout = findViewById(R.id.rootLayout);
+        
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showRegisterDialog();
+            }
+        });
+        
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showLoginDialog();
+            }
+        });
+    }
+
+    private void showLoginDialog() {
+        final AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle(R.string.sign_in);
+        dialog.setMessage(R.string.dialog_message_singIn);
+
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View login_layout = inflater.inflate(R.layout.layout_login, null);
+
+        final MaterialEditText edtEmail = login_layout.findViewById(R.id.edtEmail);
+        final MaterialEditText edtPassword = login_layout.findViewById(R.id.edtPassword);
+
+        dialog.setView(login_layout);
+
+        /* Setting dialog buttons */
+        dialog.setPositiveButton(getString(R.string.sign_in), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int which) {
+                dialogInterface.dismiss();
+
+                /* Validation check is performed here */
+                if (TextUtils.isEmpty(edtEmail.getText().toString())) {
+                    Snackbar.make(rootLayout, getString(R.string.enter_email), Snackbar.LENGTH_SHORT)
+                            .show();
+                    return;
+                }
+                if (edtPassword.getText().toString().length() < 8 && !isValidPassword(edtPassword.getText().toString())) {
+                    Snackbar.make(rootLayout, getString(R.string.invalid_pass), Snackbar.LENGTH_SHORT)
+                            .show();
+                    return;
+                }
+                if (TextUtils.isEmpty(edtPassword.getText().toString())) {
+                    Snackbar.make(rootLayout, getString(R.string.enter_pass), Snackbar.LENGTH_SHORT)
+                            .show();
+                    return;
+                }
+                if (!isValidEmailId(edtPassword.getText().toString())) {
+                    Snackbar.make(rootLayout, getString(R.string.invalid_email), Snackbar.LENGTH_SHORT)
+                            .show();
+                    return;
+                }
+
+                /* Login mechanism */
+                auth.signInWithEmailAndPassword(edtEmail.getText().toString(), edtPassword.getText().toString())
+                        .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                            @Override
+                            public void onSuccess(AuthResult authResult) {
+                                startActivity(new Intent(MainActivity.this, Welcome.class));
+                                finish();
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Snackbar.make(rootLayout, getString(R.string.incorrect_login), Snackbar.LENGTH_SHORT).show();
+                            }
+                        });
+            }
+            });
+
+        dialog.setNegativeButton(getString(R.string.cancel), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int which) {
+                dialogInterface.dismiss();
+            }
+        });
+        dialog.show();
+    }
+
+    private void showRegisterDialog() {
+        final AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle(R.string.dialog_register);
+        dialog.setMessage(R.string.dialog_message);
+
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View register_layout = inflater.inflate(R.layout.layout_register, null);
+
+        final MaterialEditText edtEmail = register_layout.findViewById(R.id.edtEmail);
+        final MaterialEditText edtPassword = register_layout.findViewById(R.id.edtPassword);
+        final MaterialEditText edtName = register_layout.findViewById(R.id.edtName);
+        final MaterialEditText edtPhone = register_layout.findViewById(R.id.edtPhone);
+
+        dialog.setView(register_layout);
+
+        /* Setting dialog buttons */
+        dialog.setPositiveButton(getString(R.string.register), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int which) {
+                dialogInterface.dismiss();
+
+                /* Validation check is performed here */
+                if(TextUtils.isEmpty(edtEmail.getText().toString())){
+                    Snackbar.make(rootLayout, getString(R.string.enter_email), Snackbar.LENGTH_SHORT)
+                            .show();
+                    return;
+                }
+                if(TextUtils.isEmpty(edtName.getText().toString())){
+                    Snackbar.make(rootLayout, getString(R.string.enter_name), Snackbar.LENGTH_SHORT)
+                            .show();
+                    return;
+                }
+                if(TextUtils.isEmpty(edtPhone.getText().toString())){
+                    Snackbar.make(rootLayout, getString(R.string.enter_phone), Snackbar.LENGTH_SHORT)
+                            .show();
+                    return;
+                }
+                if(edtPassword.getText().toString().length() < 8 && !isValidPassword(edtPassword.getText().toString())){
+                    Snackbar.make(rootLayout, getString(R.string.invalid_pass), Snackbar.LENGTH_SHORT)
+                            .show();
+                    return;
+                }
+                if(TextUtils.isEmpty(edtPassword.getText().toString())){
+                    Snackbar.make(rootLayout, getString(R.string.enter_pass), Snackbar.LENGTH_SHORT)
+                            .show();
+                    return;
+                }
+                if(!isValidEmailId(edtPassword.getText().toString())){
+                    Snackbar.make(rootLayout, getString(R.string.invalid_email), Snackbar.LENGTH_SHORT)
+                            .show();
+                    return;
+                }
+
+                /* Register new user */
+                auth.createUserWithEmailAndPassword(edtEmail.getText().toString(), edtPassword.getText().toString())
+                        .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                            @Override
+                            public void onSuccess(AuthResult authResult) {
+                                /* Save use to real-time database */
+                                User user = new User();
+                                user.setEmail(edtEmail.getText().toString());
+                                user.setEmail(edtName.getText().toString());
+                                user.setEmail(edtPhone.getText().toString());
+                                user.setEmail(edtPassword.getText().toString());
+
+                                users.child(user.getEmail())
+                                        .setValue(user)
+                                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                            @Override
+                                            public void onSuccess(Void aVoid) {
+                                                Snackbar.make(rootLayout, getString(R.string.registration_success), Snackbar.LENGTH_SHORT).show();
+                                            }
+                                        })
+                                        .addOnFailureListener(new OnFailureListener() {
+                                            @Override
+                                            public void onFailure(@NonNull Exception e) {
+                                                Snackbar.make(rootLayout, getString(R.string.registration_failed), Snackbar.LENGTH_SHORT).show();
+                                            }
+                                        });
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Snackbar.make(rootLayout, getString(R.string.registration_failed), Snackbar.LENGTH_SHORT).show();
+                            }
+                        });
+            }
+        });
+
+        dialog.setNegativeButton(getString(R.string.cancel), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int which) {
+                dialogInterface.dismiss();
+            }
+        });
+        dialog.show();
+    }
+
+    private boolean isValidPassword(final String pass){
+        Pattern pattern;
+        Matcher matcher;
+        final String PASSWORD_PATTERN = "^(?=.*[0-9])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\\S+$).{4,}$";
+        pattern = Pattern.compile(PASSWORD_PATTERN);
+        matcher = pattern.matcher(pass);
+
+        return matcher.matches();
+    }
+
+    private boolean isValidEmailId(String email){
+
+        return Pattern.compile("^(([\\w-]+\\.)+[\\w-]+|([a-zA-Z]{1}|[\\w-]{2,}))@"
+                + "((([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?"
+                + "[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\."
+                + "([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?"
+                + "[0-9]{1,2}|25[0-5]|2[0-4][0-9])){1}|"
+                + "([a-zA-Z]+[\\w-]+\\.)+[a-zA-Z]{2,4})$").matcher(email).matches();
     }
 }
